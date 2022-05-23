eeb7ae7f5bdf21d1c1842551a1c87fb88d02ee45
Vrishti Gupta <ivrishtigupta@gmail.com>
2022-05-23 21:04:40 +0530

    Using string

--- Data Structures/Recursion1/Assignment2/replace_pi.cpp
+++ Data Structures/Recursion1/Assignment2/replace_pi.cpp
@@ -22,3 +22,29 @@ int main() {
     ReplacePI(str, strlen(str));
     cout << str;
 }
+
+// Using string
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+string RPR(string str) {
+    if(str.length() == 0 || str.length() == 1) 
+        return str;
+    string ans = RPR(str.substr(1));
+    if(str[0] == 'p' && str[1] == 'i') {
+        str = "3.14" + ans.substr(1);
+        return str;
+    }
+    return str[0] + ans;
+}
+
+int main() {
+    string str;
+    getline(cin, str);
+    cout << RPR(str) << endl;
+
+    return 0;
+}
+*/
