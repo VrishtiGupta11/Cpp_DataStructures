68c502bf61320d84cb915a2c98150a2dc0171d8a
Vrishti Gupta <ivrishtigupta@gmail.com>
2022-06-03 19:10:53 +0530

    Print Subsets

--- Data Structures/Recursion2/Assignment/print_subsets.cpp
+++ Data Structures/Recursion2/Assignment/print_subsets.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+/*
 void PrintSubset(int arr[], int N, int output[], int M) {
     if(N == 0) {
         for(int i=0; i < M; i++)
@@ -19,4 +19,27 @@ int main() {
     for(int i=0; i<N; i++) 
         cin >> arr[i];
     PrintSubset(arr, N, output, 0);
+}
+*/
+
+void printSubset(int arr[], int N, int ind, int m, int output[]) {
+    if(ind == N) {
+        for(int i=0; i<m; i++) 
+            cout << output[i];
+        cout << endl;
+        return;
+    }
+    output[m] = arr[ind];
+    printSubset(arr, N, ind+1, m+1, output);
+    printSubset(arr, N, ind+1, m, output);
+}
+
+int main() {
+    int N, arr[100], output[100];
+    cin >> N;
+    for(int i=0; i<N; i++) 
+        cin >> arr[i];
+    printSubset(arr, N, 0, 0, output);
+
+    return 0;
 }
