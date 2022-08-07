a8fd02b56daf9f0c816ddf884aba22fb20c15b08
Vrishti Gupta <ivrishtigupta@gmail.com>
2022-08-07 23:11:51 +0530

    Tries

--- Data Structures/Tries and Huffman Coding/trie.h
+++ Data Structures/Tries and Huffman Coding/trie.h
@@ -37,6 +37,18 @@ class Trie {
             return false;
     }
 
+    bool search(TrieNode* root, string word) {
+        if(word.size() == 0) {
+            return true;
+        }
+        int index = word[0] - 'a';
+        if(root -> children[index] != NULL) {
+            return search(root -> children[index], word.substr(1));
+        }
+        else
+            return false;
+    }
+
     void removeWord(TrieNode* root, string word) {
         if(word.size() == 0) {
             root -> isTerminal = false;
@@ -50,6 +62,7 @@ class Trie {
         else {
             return;  // Word not found
         }
+        
         removeWord(child, word.substr(1));
 
         // Remove child if it is useless
@@ -63,6 +76,30 @@ class Trie {
         }
     }
 
+    void allPossibleNodes(TrieNode* root, string output, string word) {
+        if(root->isTerminal) 
+            cout << word + output << endl;
+        for(int i=0; i<26; i++) {
+            if(root -> children[i] != NULL) 
+                allPossibleNodes(root -> children[i], output+root -> children[i] -> data, word);
+        }
+    }
+
+    void autoComplete(TrieNode* root, string str, string word) {
+        if(str.size() == 0) {
+            allPossibleNodes(root, "", word);
+            return;
+        }
+        int index = str[0] - 'a';
+        if(root -> children[index] != NULL) {
+            autoComplete(root -> children[index], str.substr(1), word);
+        }
+        else {
+            cout << "No word found :)" << endl;
+            return;
+        }
+    }
+
 public:
     Trie() {
         root = new TrieNode('\0');
@@ -72,11 +109,22 @@ public:
         insertWord(root, word);
     }
 
+    // if looking to find Exact same word
     bool searchWord(string word) {
         return searchWord(root, word);
     }
 
     void removeWord(string word) {
         removeWord(root, word);
+    }
+
+    // Search function -> if looking to find pattern
+    bool search(string word) {
+        return search(root, word);
+    }
+
+    // Autocomplete
+    void autoComplete(string word) {
+        autoComplete(root, word, word);
     }
 };
