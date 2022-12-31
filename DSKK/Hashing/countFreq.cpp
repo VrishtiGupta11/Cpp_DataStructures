0551d0b252832ed95342c97698718b9f8a795dcf
Vrishti Gupta <53931644+VrishtiGupta11@users.noreply.github.com>
2022-12-31 18:35:28 +0530

    Update countFreq.cpp

--- DSKK/Hashing/countFreq.cpp
+++ DSKK/Hashing/countFreq.cpp
@@ -1,1 +1,25 @@
-// P0
+// P0: Count Frequency
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void countFreq(vector<int> &nums) {
+  unordered_map<int, int> mp;
+  for(auto it : nums) {
+    mp[it]++;
+  }
+  
+  for(auto it : mp) {
+    cout << it.first << " " << it.second << endl;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> nums(n);
+  for(int i=0; i<n; i++) {
+    cin >> nums[i];
+  }
+  countFreq(nums);
+}
